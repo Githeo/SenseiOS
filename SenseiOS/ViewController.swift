74356c596a37530329abfcada647ecbfa8cf420d
Matteo Sammarco <matteo.sammarco@axa-groupsolutions.com>
2016-06-08 18:58:40 +0200

    Getting experiment ID

--- SenseiOS/ViewController.swift
+++ SenseiOS/ViewController.swift
@@ -16,8 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     
     let motionKit = MotionKit();
-    let samplingFrequency = 0.1 //0.1=10Hz, 0.01=100Hz
+    let samplingFrequency = 1.0 // 0.1 = 10Hz, 0.01 = 100Hz
+    let gravity = 9.81
     var isRecording = false
+    var experimentName:String = "" // Should be like this 2016_04_15-16_52.csv
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,19 +32,45 @@ class ViewController: UIViewController {
     }
 
     func startCollection(){
+        print("EXPERIMENT: \(experimentName)")
         motionKit.getAccelerometerValues(samplingFrequency) { (x, y, z) in
-            print("ACCELEROMETER X: \(x) Y: \(y) Z \(z)")
+            print("IOS ACCELEROMETER X: \(x) Y: \(y) Z: \(z)")
             // Unit=G, gravitation force equal to that exerted by the earth’s gravitational field (9.81 m s−2)
         }
         motionKit.getGyroValues(samplingFrequency){ (x, y, z) in
-            print("GYRO X: \(x) Y: \(y) Z \(z)")
+            print("IOS GYRO X: \(x) Y: \(y) Z: \(z)")
             // Unit=radiant/sec
         }
         motionKit.getMagnetometerValues(samplingFrequency){
             (x, y, z) in
-            print("MAGNETOMETER X: \(x) Y: \(y) Z \(z)")
+            print("IOS MAGNETOMETER X: \(x) Y: \(y) Z: \(z)")
+            // Unit:
         }
-        
+        motionKit.getDeviceMotionObject(samplingFrequency){
+            (deviceMotion) -> () in
+            let accelerationX = deviceMotion.userAcceleration.x
+            let accelerationY = deviceMotion.userAcceleration.y
+            let accelerationZ = deviceMotion.userAcceleration.z
+            print("IOS DM ACCELERATION X: \(accelerationX) Y: \(accelerationY) Z: \(accelerationZ)")
+            // Unit = G. It's the linear acceleration
+            
+            let gravityX = deviceMotion.gravity.x
+            let gravityY = deviceMotion.gravity.y
+            let gravityZ = deviceMotion.gravity.z
+            print("IOS GRAVITY X: \(gravityX) Y: \(gravityY) Z: \(gravityZ)")
+            // Unity = G
+            
+            let rotationX = deviceMotion.rotationRate.x
+            let rotationY = deviceMotion.rotationRate.y
+            let rotationZ = deviceMotion.rotationRate.z
+            print("IOS ROTATION X: \(rotationX) Y: \(rotationY) Z: \(rotationZ)")
+            
+            //var magneticFieldX = deviceMotion.magneticField.x
+            //var attitideYaw = deviceMotion.attitude.yaw
+        }
+        motionKit.getAttitudeFromDeviceMotion(samplingFrequency) { (attitude) in
+            print ("IOS ROTATION MATRIX: \(attitude.rotationMatrix)") // 3x3 matrix
+        }
     }
     
     func stopCollection(){
@@ -52,16 +80,25 @@ class ViewController: UIViewController {
         motionKit.stopDeviceMotionUpdates()
     }
     
+    func getExperimentDate() -> String{
+        let todaysDate:NSDate = NSDate()
+        let dateFormatter:NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy_MM_dd-HH_mm_ss"
+        let dateInFormat:String = dateFormatter.stringFromDate(todaysDate)
+        return dateInFormat
+    }
+    
     // MARK: Actions
     @IBAction func startStopButtonAction(sender: UIButton) {
-        if (isRecording){
-            print("Button: is recording. Stopping")
+        if (isRecording){ // STOPPING
             isRecording = false
             startButton.backgroundColor = UIColor.greenColor()
             stopCollection()
-        } else {
-            print("Button: not recording. Starting")
+            monitorTextView.text = ""
+        } else { // STARTING
             isRecording = true
+            experimentName = getExperimentDate()
+            monitorTextView.text = experimentName
             startButton.backgroundColor = UIColor.redColor()
             startCollection()
         }
