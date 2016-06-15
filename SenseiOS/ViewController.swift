aeba3d42839933ad2ed0b01ff768dc7a567603a9
Matteo Sammarco <matteo.sammarco@axa-groupsolutions.com>
2016-06-15 18:05:06 +0200

    Saving rotation matrix, angles, gyro, magnetometer, acceleration to file

--- SenseiOS/ViewController.swift
+++ SenseiOS/ViewController.swift
@@ -17,8 +17,11 @@ class ViewController: UIViewController {
     
     let motionKit = MotionKit();
     let samplingFrequency = 0.1 // 0.1 = 10Hz, 0.01 = 100Hz
-    let gravity = 9.81
+    let GRAVITY = 9.81
     let CSV_SEP = ";"
+    let OPEN_DATA_ARRAY = "["   // to group arrays. e.g., [x, y, z]
+    let CLOSE_DATA_ARRAY = "]"  // to group arrays. e.g., [x, y, z]
+    let DATA_SEP = ", "         // separator for data in an array. e.g., x, y, z
     var isRecording = false
     var experimentName:String = "" // Should be like this 2016_04_15-16_52.csv
     var csvOutputFileName = ""
@@ -42,19 +45,25 @@ class ViewController: UIViewController {
     func startCollection(){
         print("EXPERIMENT: \(experimentName)")
         
+        // ------- ACCELEROMETER ---------//
+        // Unit = normally is in G, I convert in m/s^2. This includes gravity.
+        // Timestamp unit is milliseconds. e.g., 1466001929014.57
         motionKit.getAccelerometerValues(samplingFrequency) { (x, y, z) in
-            print(" IOS ACCELEROMETER X: \(x) Y: \(y) Z: \(z)")
-            // Unit=G, gravitation force equal to that exerted by the earth’s gravitational field (9.81 m s−2)
+            self.writeDataToFile("IOS Accelerometer\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(x * self.GRAVITY)\(self.DATA_SEP) \(y * self.GRAVITY)\(self.DATA_SEP) \(z * self.GRAVITY)\(self.CLOSE_DATA_ARRAY)\n" )
+            //print("IOS Accelerometer\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(x * self.GRAVITY)\(self.DATA_SEP)\(y * self.GRAVITY)\(self.DATA_SEP)\(z * self.GRAVITY)\(self.CLOSE_DATA_ARRAY)")
         }
+        // --------- GYROSCOPE -----------//
+        // Unit = radiant/sec
         motionKit.getGyroValues(samplingFrequency){ (x, y, z) in
-            print("IOS GYRO\(self.CSV_SEP)\((NSDate().timeIntervalSince1970 * 1000))\(self.CSV_SEP) X: \(x) Y: \(y) Z: \(z)")
-            // Unit=radiant/sec
+            self.writeDataToFile("IOS Gyroscope\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(x)\(self.DATA_SEP) \(y)\(self.DATA_SEP) \(z)\(self.CLOSE_DATA_ARRAY)\n")
+            //print("IOS Gyroscope\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(x)\(self.DATA_SEP)\(y)\(self.DATA_SEP)\(z)\(self.CLOSE_DATA_ARRAY)\n")
         }
+        // -------- MAGNETOMETER ---------//
+        // Unit = microTesla
         motionKit.getMagnetometerValues(samplingFrequency){
             (x, y, z) in
-            print("IOS MAGNETOMETER X:\(x) Y: \(y) Z: \(z)")
-            self.writeDataToFile("IOS MAGNETOMETER X: \(x) Y: \(y) Z: \(z)\n")
-            // Unit:
+            self.writeDataToFile("IOS Magnetometer\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(x)\(self.DATA_SEP) \(y)\(self.DATA_SEP) \(z)\(self.CLOSE_DATA_ARRAY)\n")
+            // print("IOS Magnetometer\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(x * self.GRAVITY)\(self.DATA_SEP)\(y * self.GRAVITY)\(self.DATA_SEP)\(z * self.GRAVITY)\(self.CLOSE_DATA_ARRAY)\n")
         }
         motionKit.getDeviceMotionObject(samplingFrequency){
             (deviceMotion) -> () in
@@ -75,11 +84,19 @@ class ViewController: UIViewController {
             let rotationZ = deviceMotion.rotationRate.z
             print("IOS ROTATION X: \(rotationX) Y: \(rotationY) Z: \(rotationZ)")
             
+            print("IOS Pitch-roll-yawn \(deviceMotion.attitude.pitch) \(deviceMotion.attitude.roll) \(deviceMotion.attitude.yaw)")
             //var magneticFieldX = deviceMotion.magneticField.x
             //var attitideYaw = deviceMotion.attitude.yaw
         }
+        // ------ ROTATION MATRIX -------- //
+        // 3x3 [11, 12, 13, 21, 22, 23, 31, 32, 33]
         motionKit.getAttitudeFromDeviceMotion(samplingFrequency) { (attitude) in
-            print ("IOS ROTATION MATRIX: \(attitude.rotationMatrix)") // 3x3 matrix
+            self.writeDataToFile("IOS Rotation Matrix\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(attitude.rotationMatrix.m11)\(self.DATA_SEP) \(attitude.rotationMatrix.m12)\(self.DATA_SEP) \(attitude.rotationMatrix.m13)\(self.DATA_SEP) \(attitude.rotationMatrix.m21)\(self.DATA_SEP) \(attitude.rotationMatrix.m22)\(self.DATA_SEP) \(attitude.rotationMatrix.m23)\(self.DATA_SEP) \(attitude.rotationMatrix.m31)\(self.DATA_SEP) \(attitude.rotationMatrix.m32)\(self.DATA_SEP) \(attitude.rotationMatrix.m33)\(self.CLOSE_DATA_ARRAY)")
+            self.writeDataToFile("IOS PITCH-ROLL-YAW\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(NSDate().timeIntervalSince1970 * 1000)\(self.CSV_SEP)\(self.OPEN_DATA_ARRAY)\(attitude.pitch)\(self.DATA_SEP) \(attitude.roll)\(self.DATA_SEP) \(attitude.yaw)\(self.CLOSE_DATA_ARRAY)")
+            // print("PITCH: \(attitude.pitch) \(attitude.roll) \(attitude.yaw))")
+        }
+        motionKit.getAccelerationFromDeviceMotion(samplingFrequency) { (x, y, z) in
+            print("DM acceleration \(x) \(y) \(z)")
         }
     }
     
